6c6c09baa40857e8b22f84b131422acf26691086
GithubMD78 <talk2manu4u@yahoo.com>
2018-04-19 23:28:07 +0530

    My project setup complete.

---
+++ mdshoppingbackend/src/main/java/net/mmd/mdshoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.mmd.mdshoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
